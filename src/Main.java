621ba3128948abf15552c0b81a7f583133aee7ee
markup54 <marzenakupka@tlen.pl>
2022-09-15 08:26:00 +0200

    klonowanie obiektow

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,41 @@ public class Main {
         //tablica wylosowane
         int wylosowane[] = new int [6];
         //wartości w tablicy typu prostego lub złożonego
+        //w tablicy nie ma możliwości zmiany jej rozmiaru po jej deklaracji
         for(int i=0;i< wylosowane.length;i++){
             wylosowane[i]=(int)(Math.random()*100+1);
         }
         for (int element : wylosowane){
             System.out.print(element+", ");
         }
-
+        int wylosowana2 [] ;
+        wylosowana2 = wylosowane;
+        wylosowane[0] = 0;
+        System.out.println();
+        System.out.println("tablica 1");
+        for (int element : wylosowane){
+            System.out.print(element+", ");
+        }
+        System.out.println("tablica 2");
+        for (int element : wylosowana2){
+            System.out.print(element+", ");
+        }
+        System.out.println(wylosowane);
+        System.out.println(wylosowana2);
+        int wylosowane3[];
+        wylosowane3=wylosowane.clone();
+        wylosowane[1]=0;
+        System.out.println("tablica 1");
+        for (int element : wylosowane){
+            System.out.print(element+", ");
+        }
+        System.out.println("tablica 2");
+        for (int element : wylosowana2){
+            System.out.print(element+", ");
+        }
+        System.out.println("tablica 3");
+        for (int element : wylosowane3){
+            System.out.print(element+", ");
+        }
     }
 }
